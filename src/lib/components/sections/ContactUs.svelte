3af1d210fbf4af5733d2b7207364f68dbb8bf32e
J4CKT-01 <jackstolt@protonmail.com>
2025-03-12 10:11:10 +0000

    Contact Form Responsiveness

--- src/lib/components/sections/ContactUs.svelte
+++ src/lib/components/sections/ContactUs.svelte
@@ -5,9 +5,9 @@
 <section>
 	<div class="container mx-auto mt-5 lg:mt-15 mb-5 lg:mb-15 flex p-2 lg:p-6 flex-col">
         <div class="w-full text-center"><Header>Contact Us!</Header></div>
-        <div class="flex flex-col lg:flex-row mt-10 lg:mt-15">
-            <div class="mb-5 lg:mb-0 w-full lg:w-1/2 flex justify-center lg:justify-start">
-                <div class="flex flex-col justify-evenly border rounded-3xl space-y-20 h-full w-full lg:w-3/4 p-10">
+        <div class="flex flex-col justify-evenly lg:flex-row mt-10 lg:mt-15">
+            <div class="mb-5 lg:mb-0 w-full lg:w-2/5 flex justify-center lg:justify-start">
+                <div class="flex flex-col justify-evenly border rounded-3xl space-y-20 h-full w-full p-10">
                     <div class="flex flex-col items-center">
                         <i class="fa-sharp-duotone fa-regular fa-envelope text-5xl mb-5"></i>
                         <p class="text-2xl">info@amalfiiheating.co.uk</p>
@@ -23,7 +23,7 @@
                 </div>
             </div>
             <div class="w-full lg:hidden text-center"><Header>Or...</Header></div>
-            <div class="w-full lg:w-1/2">
+            <div class="w-full lg:w-2/5">
                 <div class="powr-form-builder" id="b8ea7ad7_1737553893"></div><script src="https://www.powr.io/powr.js?platform=html"></script>
             </div>
         </div>
