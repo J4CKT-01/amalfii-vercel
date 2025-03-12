65fe1215e15d8899d78ebb8eaa50fd953ca22b71
J4CKT-01 <jackstolt@protonmail.com>
2025-03-12 10:07:47 +0000

    additional styling 2

--- src/lib/components/sections/ContactUs.svelte
+++ src/lib/components/sections/ContactUs.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <section>
-	<div class="container mx-auto mt-15 mb-15 flex p-6 flex-col">
+	<div class="container mx-auto mt-5 lg:mt-15 mb-5 lg:mb-15 flex p-2 lg:p-6 flex-col">
         <div class="w-full text-center"><Header>Contact Us!</Header></div>
-        <div class="flex flex-col lg:flex-row mt-25">
-            <div class="mb-15 lg:mb-0 w-full lg:w-1/2 flex justify-center lg:justify-start">
-                <div class="flex flex-col justify-evenly shadow-xl space-y-20 h-full w-full lg:w-3/4 p-10">
+        <div class="flex flex-col lg:flex-row mt-10 lg:mt-15">
+            <div class="mb-5 lg:mb-0 w-full lg:w-1/2 flex justify-center lg:justify-start">
+                <div class="flex flex-col justify-evenly border rounded-3xl space-y-20 h-full w-full lg:w-3/4 p-10">
                     <div class="flex flex-col items-center">
                         <i class="fa-sharp-duotone fa-regular fa-envelope text-5xl mb-5"></i>
                         <p class="text-2xl">info@amalfiiheating.co.uk</p>
